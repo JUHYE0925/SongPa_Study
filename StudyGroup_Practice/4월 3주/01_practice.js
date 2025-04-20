2e66daea0255c93a71198a915c3fc74dbeeea8c4
DESKTOP-43RUNA1\USER <rhkdlf48301@gmail.com>
2025-04-20 17:11:03 +0900

    April_Week3_Study_Group_Practice

---
+++ StudyGroup_Practice/4월 3주/01_practice.js
@@ -0,0 +1,24 @@
+/* 시은님 문제 */
+
+/*
+다음 배열이 주어졌을 때:
+arr = [1, 2, 3]
+
+이 배열을 아래와 같이 변경해보세요:
+arr = [3, 4, 5, 6, 7]
+*/
+
+// for문 사용해보기!
+
+let arr = [1, 2, 3];
+
+arr.shift();
+arr.shift();
+
+console.log(arr);
+
+for(let i = 4; i < 8; i++){
+    arr.push(i);
+}
+
+console.log(arr);
---
+++ StudyGroup_Practice/4월 3주/02_practice.js
@@ -0,0 +1,13 @@
+/* 주혜 문제 */
+
+// 1. 50부터 100까지의 숫자를 랜덤으로 5개를 뽑은 후 내림차순으로 정렬한 배열을 구하시오.
+
+let arr = [];
+for(let i = 0; i < 5; i++){
+    arr[i] = Math.floor(Math.random() * (100 - 50 + 1)) + 50;
+}
+
+console.log(arr);
+const desc = arr.sort((a, b) => b - a);
+console.log(desc);
+
