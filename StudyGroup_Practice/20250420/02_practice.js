57ebd32402870ae28446bde6ce4f26a15fe94f21
DESKTOP-43RUNA1\USER <rhkdlf48301@gmail.com>
2025-05-02 00:45:34 +0900

    no message

--- StudyGroup_Practice/20250420/02_practice.js
+++ StudyGroup_Practice/20250420/02_practice.js
@@ -1,3 +1,5 @@
+/* 주혜 문제 */
+
 // 1. 50부터 100까지의 숫자를 랜덤으로 5개를 뽑은 후 내림차순으로 정렬한 배열을 구하시오.
 
 let arr = [];
@@ -6,6 +8,8 @@ for(let i = 0; i < 5; i++){
 }
 
 console.log(arr);
+
 const desc = arr.sort((a, b) => b - a);
+
 console.log(desc);
 
